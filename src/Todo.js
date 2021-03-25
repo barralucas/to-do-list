4155c84a516bf151edb276fce598b1c1e3c699b4
Lucas Barra <lucastbarra@hotmail.com>
2021-03-25 17:03:24 -0300

    first commit

--- src/Todo.js
+++ src/Todo.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import './Todo.css'
 
-function Todo(){
+function Todo() {
 
-    return(
+    return (
         <div className="container">
-        <h1>To do</h1>
-        <form>
-            <input type="text"></input>
-            <button>add</button>
-        </form>
+            <h1>To do</h1>
+            <form>
+                <input type="text"></input>
+                <button>add</button>
+            </form>
 
-        <ul>
-            <li>item 1</li>
-        </ul>
-
-
+            <ul>
+                <li>item 1</li>
+            </ul>
         </div>
     );
 
