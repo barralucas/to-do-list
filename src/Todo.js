fcf2caf958a19e8c899e73cdbafc7dbd153933c3
Lucas Barra <lucastbarra@hotmail.com>
2021-03-25 16:49:12 -0300

    first commit

---
+++ src/Todo.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Todo.css'
+
+function Todo(){
+
+    return(
+        <div className="container">
+        <h1>To do</h1>
+        <form>
+            <input type="text"></input>
+            <button>add</button>
+        </form>
+
+        <ul>
+            <li>item 1</li>
+        </ul>
+
+
+        </div>
+    );
+
+
+}
+
+
+export default Todo;
