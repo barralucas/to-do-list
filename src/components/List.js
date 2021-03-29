753f18bcbb9ea41349f596d78d2ac618f8c5480b
Lucas Barra <lucastbarra@hotmail.com>
2021-03-29 10:51:35 -0300

    separando mais os componentes

--- src/components/List.js
+++ src/components/List.js
@@ -1,35 +1,12 @@
 import React from 'react';
-import Card from './Card';
-
-
-function DoneImg(props) {
-
-    if (props.done) {
-        return (<img alt="done" src="./assets/done.png"></img>)
-    } else {
-        return (<img alt="in-progress" src="./assets/loading.png"></img>)
-    }
-
-}
+import ListItem from './ListItem';
 
 
 function List(props) {
 
     return (
         <ul>
-            {props.items.map(item => <li key={item.id}>
-                <Card className={item.done ? "done item" : "item"}>
-                    {item.text}
-                    <div>
-                        <button onClick={() => { props.onDone(item) }}>
-                            <DoneImg done={item.done}></DoneImg>
-                        </button>
-                        <button onClick={() => { props.onItemDeleted(item) }}>
-                            <img alt="apagar" src="./public/assets/lixeira.png"></img>
-                        </button>
-                    </div>
-                </Card>
-            </li>)}
+            {props.items.map(item => <ListItem key={item.id} item={item} onDone={props.onDone} onItemDeleted={props.onItemDeleted}></ListItem>)}
         </ul>
     );
 }
